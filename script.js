ee6ef350ba7ed90faa4c4403339b83a777a400d7
deafnv <rokiniri@yahoo.in>
2022-10-22 05:17:06 +0800

    feat: draggable emote list and visible poll modal

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ $("#controlsrow").appendTo($(".nano-content"));
 $("#playlistrow").appendTo($(".nano-content"));
 $("#sitefooter").appendTo($(".nano-content"));
 $("#footer").appendTo($(".nano-content"));
+$(".leftcontent").prepend($("#pollwrap"));
+
+$('<div class="emotewrap" id="emotewrap">').appendTo($(".rightcontent"));
 
 const chatline = document.getElementById("chatline");
 chatline.removeAttribute("placeholder");
@@ -160,7 +163,7 @@ function showEmotes() {
     EMOTES = true;
 }
 
-$("#emotespanel").appendTo($(".rightcontent"));
+$("#emotespanel").appendTo($(".emotewrap"));
 
 $("#emotelistbtn").remove();
 
@@ -191,3 +194,46 @@ $('<button id="clear-btn" class="btn btn-default btn-sm">Clear</button>')
             msg: '/clear'
         });
     });
+
+    dragElement(document.getElementById("emotewrap"));
+
+    function dragElement(elmnt) {
+      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+      if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+      } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+      }
+    
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+    
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      }
+    
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
