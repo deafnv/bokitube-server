98cc91e88980d706cfeff3df09d05f5d33615973
deafnv <rokiniri@yahoo.in>
2022-10-22 18:13:36 +0800

    style: improve JS readability

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+/* Removes the buttons for resizing video and user list size toggle */
 const resizes = document.getElementById("resize-video-smaller");
 const resizel = document.getElementById("resize-video-larger");
 const userlisttoggle = document.getElementById("userlisttoggle");
-
 resizes.remove();
 resizel.remove();
 userlisttoggle.remove();
 
+/* Adds scrolling banner to MOTD wrap */
 $("#motdwrap").prepend($('<div class="banner-slideshow"><div class="mover-1"></div></div>'));
+
+/* Theme credits */
 $(".credit").append($('<p class="text-muted credit">Theme by TomoLover, available on <a href="https://github.com/deafnv/bokigang-server" target="_blank" rel="noreferrer noopener">Github</a></p><p class="text-muted credit">Breast milk (sometimes spelled as breastmilk) or mother\'s milk is milk produced by mammary glands, located in the breast of a human female. Breast milk is the primary source of nutrition for newborns, containing fat, protein, carbohydrates (lactose and human milk oligosaccharides) and variable minerals and vitamins. Breast milk also contains substances that help protect an infant against infection and inflammation, whilst also contributing to healthy development of the immune system and gut microbiome.</p>'));
 
-$("#mainpage").prepend($("#chatwrap"));
+/* Create basic two column layout */
 $("#mainpage").prepend($('<div class="rightcontent">'))
 $("#mainpage").prepend($('<div class="leftcontent">'))
 
+/* Place video and the rest of the page into the left column */
 $("<div id='video-container'>").prependTo($(".leftcontent"));
 $("#videowrap").prependTo($("#video-container"));
-$("#chatwrap").appendTo($(".rightcontent"));
-$("#leftcontrols").appendTo($("#chatwrap"));
-
 $('<div class="nano-content">').appendTo($(".leftcontent"));
 $("#motdrow").appendTo($(".nano-content"));
 $("#controlsrow").appendTo($(".nano-content"));
@@ -26,8 +27,12 @@ $("#sitefooter").appendTo($(".nano-content"));
 $("#footer").appendTo($(".nano-content"));
 $(".leftcontent").prepend($("#pollwrap"));
 
+/* Place chat and buttons into the right column */
+$("#chatwrap").appendTo($(".rightcontent"));
+$("#leftcontrols").appendTo($("#chatwrap"));
 $('<div class="emotewrap" id="emotewrap">').appendTo($(".rightcontent"));
 
+/* Meant for implementation of scrolling title - remove if unused */
 $("#chatwrap").prepend($("<div class='currenttitlewrap'>"));
 $("#videowrap-header").prependTo($(".currenttitlewrap"));
 
@@ -36,7 +41,7 @@ const clone = node.cloneNode(true);
 $("#videowrap-header").append($("<span> </span>"));
 document.getElementById("videowrap-header").appendChild(clone);
 
-
+/* Add hint text for chatline and disables spellcheck */
 const chatline = document.getElementById("chatline");
 chatline.removeAttribute("placeholder");
 chatline.setAttribute("placeholder", "Send a message");
@@ -45,6 +50,7 @@ chatline.setAttribute("spellcheck", "false");
 
 
 //OLDER CODE: DON'T TOUCH
+/* AFK on unfocus function */
 var VOL_AFK = false;
 var FOCUS_AFK = false;
 setInterval(function() {
@@ -76,6 +82,7 @@ setInterval(function() {
     }
 }, 500);
 
+/* Adds favicon and externally hosted fonts from Google Fonts */
 $(document).ready(function() {
     $('<link id="chanfavicon" href="https://dl.dropboxusercontent.com/s/8a079qbl5cr3zgu/youbdayfavi.png" type="image/x-icon" rel="shortcut icon" />').appendTo("head");
 
@@ -84,8 +91,10 @@ $(document).ready(function() {
     $('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand">').appendTo("head");
 });
 
+/* Navbar title */
 $(".navbar-brand").html("BOKIGANG");
 
+/* Adds CSS preview button to built-in CSS editor */
 $('<button class="btn btn-primary" id="cs-csspreview">Preview CSS</button>')
     .appendTo("#cs-csseditor")
     .on("mousedown", function() {
@@ -102,6 +111,8 @@ $('<button class="btn btn-primary" id="cs-csspreview">Preview CSS</button>')
         document.getElementById("cs-csseditor").style.visibility = "visible";
     });
 
+/* Custom emotes panel */
+/* Basic implementation: Full credits to https://github.com/zimny-lech/CyTube-Plus */
 var EMOTES = false;
 GroupEmotes_Number = 100;
 UI_ChannelCache = 1;
@@ -171,10 +182,13 @@ function showEmotes() {
     EMOTES = true;
 }
 
+/* PLace emotes panel in a wrap */
 $("#emotespanel").appendTo($(".emotewrap"));
 
+/* Remove original emote button */
 $("#emotelistbtn").remove();
 
+/* Add custom emotes panel button */
 emotesbtn = $('<button id="emotes-btn" class="btn btn-sm btn-default" title="Display emotes panel">Emote List</button>')
     .appendTo("#leftcontrols")
     .on("click", function() {
@@ -182,10 +196,12 @@ emotesbtn = $('<button id="emotes-btn" class="btn btn-sm btn-default" title="Dis
         (UI_ChannelCache != "1" && !EMOTES) ? showEmotes(): '';
     });
 
+/* Create space to align AFK and Clear buttons to the right - remove if necessary with better implementation */
 $('<button id="spacer-btn" class="btn btn-default btn-sm">Spacer button</button>')
     .appendTo("#leftcontrols")
     .on("click", function() {});
 
+/* Add custom AFK button for manual AFK */
 $('<button id="afk-btn" class="btn btn-default btn-sm">AFK</button>')
     .appendTo("#leftcontrols")
     .on("click", function() {
@@ -195,6 +211,7 @@ $('<button id="afk-btn" class="btn btn-default btn-sm">AFK</button>')
         VOL_AFK = !VOL_AFK;
     });
 
+/* Add button to clear chat */
 $('<button id="clear-btn" class="btn btn-default btn-sm">Clear</button>')
     .appendTo("#leftcontrols")
     .on("click", function() {
@@ -203,45 +220,46 @@ $('<button id="clear-btn" class="btn btn-default btn-sm">Clear</button>')
         });
     });
 
-    dragElement(document.getElementById("emotewrap"));
+/* Makes the custom emotes panel draggable */
+dragElement(document.getElementById("emotewrap"));
 
-    function dragElement(elmnt) {
-      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      if (document.getElementById(elmnt.id + "header")) {
-        // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-      } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
-        elmnt.onmousedown = dragMouseDown;
-      }
-    
-      function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-      }
-    
-      function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
-    
-      function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-      }
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
     }
+
+    function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+    }
+}
