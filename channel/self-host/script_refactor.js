7a0c4c5e9c5c0194ca3ada1629cba304cc577ec8
deafnv <rokiniri@yahoo.in>
2022-11-16 11:21:29 +0800

    feat: better autocomplete with regex

--- channel/self-host/script_refactor.js
+++ channel/self-host/script_refactor.js
@@ -295,94 +295,81 @@ function dragElement(elmnt) {
 }
 
 function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
     var currentFocus;
-    /*execute a function when someone writes in the text field:*/
+    var currentInputVal = '';
+    var matchedlength = 0;
     inp.addEventListener("input", function(e) {
         var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
+
         closeAllLists();
+        
         if (!val) { return false;}
         currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
+
         a = document.createElement("DIV");
         a.setAttribute("id", "autocomplete-list");
         a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         $("#autocomplete-list").insertBefore(document.querySelectorAll('form')[1]);
 
-        /*for each item in the array...*/
+        var matched = document.getElementById("chatline").value.match(/(?<!\S)\/\S*$/gim).toString();
+        currentInputVal = document.getElementById("chatline").value;
+
         for (i = 0; i < arr.length; i++) {
-          /*check if the item starts with the same letters as the text field value:*/
-          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
-            b.addEventListener("click", function(e) {
-                /*insert the value for the autocomplete text field:*/
-                inp.value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
-                closeAllLists();
-            });
-            a.appendChild(b);
-          }
+            if (arr[i].substr(0, matched.length).toUpperCase() == matched.toUpperCase()) {
+                matchedlength = matched.length;
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, matched.length) + "</strong>";
+                b.innerHTML += arr[i].substr(matched.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.addEventListener("click", function(e) {
+                    $("#chatline").val($("#chatline").val() + this.getElementsByTagName("input")[0].value.substring(matched.length));
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
         }
     });
-    /*execute a function presses a key on the keyboard:*/
+    
     inp.addEventListener("keydown", function(e) {
         var x = document.getElementById("autocomplete-list");
         if (x) x = x.getElementsByTagName("div");
+        
         if (e.keyCode == 40) {
-            /*If the arrow DOWN key is pressed,
-            increase the currentFocus variable:*/
             e.preventDefault();
             currentFocus++;
-            /*and and make the current item more visible:*/
             addActive(x);
+
             document.querySelector('.autocomplete-active').scrollIntoViewIfNeeded();
-            $("#chatline").val(document.getElementsByClassName('autocomplete-active')[0].querySelector('input').getAttribute('value'));
-        } else if (e.keyCode == 38) { //up
-            /*If the arrow UP key is pressed,
-            decrease the currentFocus variable:*/
+            $("#chatline").val(currentInputVal + document.getElementsByClassName('autocomplete-active')[0].querySelector('input').getAttribute('value').substring(matchedlength));
+        } else if (e.keyCode == 38) {
             e.preventDefault();
             currentFocus--;
-            /*and and make the current item more visible:*/
             addActive(x);
+
             document.querySelector('.autocomplete-active').scrollIntoViewIfNeeded();
-            $("#chatline").val(document.getElementsByClassName('autocomplete-active')[0].querySelector('input').getAttribute('value'));
+            $("#chatline").val(currentInputVal + document.getElementsByClassName('autocomplete-active')[0].querySelector('input').getAttribute('value').substring(matchedlength));
         } else if (e.keyCode == 13) {
-            /*If the ENTER key is pressed, prevent the form from being submitted,*/
             e.preventDefault();
             e.stopPropagation();
         }
     });
+
     function addActive(x) {
-        /*a function to classify an item as "active":*/
         if (!x) return false;
-        /*start by removing the "active" class on all items:*/
         removeActive(x);
         if (currentFocus >= x.length) currentFocus = 0;
         if (currentFocus < 0) currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
         x[currentFocus].classList.add("autocomplete-active");
     }
+
     function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
         for (var i = 0; i < x.length; i++) {
             x[i].classList.remove("autocomplete-active");
         }
     }
+
     function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-        except the one passed as an argument:*/
         var x = document.getElementsByClassName("autocomplete-items");
         for (var i = 0; i < x.length; i++) {
             if (elmnt != x[i] && elmnt != inp) {
@@ -390,11 +377,11 @@ function autocomplete(inp, arr) {
             }
         }
     }
-    /*execute a function when someone clicks in the document:*/
+
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
     });
 }
 
-/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
-autocomplete(document.getElementById("chatline"), autocompleteArr);
+// Disable autocomplete on mobile, performance is really bad right now, with or without
+if (!window.matchMedia("(max-width: 768px)").matches) {autocomplete(document.getElementById("chatline"), autocompleteArr);}
